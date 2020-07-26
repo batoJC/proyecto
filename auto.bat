095b739719c597b9ca13f08acb14a9d656936331
Juan Carlos <juacagiri@gmail.com>
2020-07-25 22:07:15 -0500

    nuevo

---
+++ auto.bat
@@ -0,0 +1,35 @@
+@echo off
+setlocal
+echo What do you do?
+set/p task=:
+set "var1=%task:-="^&REM #%
+set "var2=%task:*-=%"
+
+IF "%var1%"=="s" goto init    
+IF "%var1%"=="p" goto push
+IF "%var1%"=="c" goto commit
+
+%task%
+goto exit
+
+:init
+    \xampp\xampp_start.exe
+    start php artisan serve
+    code .
+    http://127.0.0.1:8000
+    goto exit
+
+:push
+    git push
+    goto exit
+
+:commit
+    git add .
+    git commit -m "%var2%"
+    goto exit
+
+
+:exit
+    pause
+    cls
+    auto.bat
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit9e2b5a0a24084850f7a95517c6858a00
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
