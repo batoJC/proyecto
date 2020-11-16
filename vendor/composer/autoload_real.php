4259cc64ef09833f1fcbbab91e16aceed554861e
Camilo Andres <camian6@outlook.com>
2020-11-16 11:17:52 -0500

    Init repo in the Camilo workspace
    what: init repo and update dependecies
    why: for the init work

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit9e2b5a0a24084850f7a95517c6858a00
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit9e2b5a0a24084850f7a95517c6858a00', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit9e2b5a0a24084850f7a95517c6858a00', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit9e2b5a0a24084850f7a95517c6858a00::getInitializer($loader));
         } else {
